b494b5fedcc39d729873d22754fa52cc99503dae
K Mak <kumarajiva08@gmail.com>
2019-04-01 23:06:49 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+
+class Snake extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 100,
+      y: 175,
+    }
+ }
+  componentDidMount() {
+    // this.interval = setInterval(() => this.mv(this.state), 1000);
+    this.interval = setInterval(() => this.mv(this.state), 50);
+  }
+  mv = (state) => {
+    this.setState({
+      x: state.x + 5,
+      y: state.y + 2,
+    })
+    if (this.state.y < 0 || this.state.y > 320) {
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    console.log(this.state);
+    let style = {
+      width: "1em",
+      height: "1em",
+      position: "absolute",
+      left: this.state.x,
+      top: this.state.y,
+      backgroundColor: "red",
+    }
+    return (
+      <div>
+        <div style={style}>
+        </div>
+      </div>
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        <Snake />
       </div>
     );
   }
